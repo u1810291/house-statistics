ebdcf9dde2fc9ebaa31e1be5eb21e738ceee075e
u1810291 <o.rizakulov@student.inha.uz>
2021-02-05 09:10:24 +0500

    service added

---
+++ src/app/services/person.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
+import { Card } from '../card';
+import { PEOPLE } from '../data/mock-people';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PeopleService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Card[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('PeopleService: fetched heroes');
+    return of(PEOPLE);
+  }
+}
